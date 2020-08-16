f9027810873f2d52da3e9c81675151c318226846
iftekhar <iftysoftwares02@gmail.com>
2020-08-16 19:22:51 +0600

    add employee function added

--- models/admin.js
+++ models/admin.js
@@ -13,5 +13,15 @@ module.exports = {
                 callback([]);
             }
         });
-    }
+    },
+    addEmployee: function(value, callback){
+        var sql="INSERT INTO `login_cred` (`username`, `password`, `type`) VALUES ('"+value.username+"','"+value.password+"','employee');";
+        db.getResults(sql,function(result){
+            var sql1="INSERT INTO `personal_details`(`u_id`, `name`, `phone`, `gender`, `designation`,`username`) VALUES ('"+result.insertId+"','"+value.name+"','"+value.phone+"','"+value.gender+"','"+value.designation+"','"+ value.username +"');";
+            console.log(sql1);
+            db.getResults(sql1,function(result1){
+                callback(result1);
+            });
+        });
+    },
 }
